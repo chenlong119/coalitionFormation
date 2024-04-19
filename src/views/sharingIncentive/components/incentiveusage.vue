816d69580abe64d1f2526045c7c4b18ad34f6e7b
perfume-g <3080807552@qq.com>
2024-04-20 00:16:06 +0800

    数据共享模块增加

---
+++ src/views/sharingIncentive/components/incentiveusage.vue
@@ -0,0 +1,138 @@
+<template>
+  <body>
+    <div>当前积分：30分
+     </div> 
+    <!-- <div>
+      <el-main>
+          <el-table :data="tableData" border stripe :header-cell-class-name="headerBg" @selection-change="handleSelectionChange">
+          <el-table-column prop="itemName" label="项目名称" width="280">
+          </el-table-column>
+          <el-table-column prop="cost" label="积分消耗(分)" width="220">
+          </el-table-column>
+          <el-table-column prop="numberleft" label="剩余份数" width="220">
+          </el-table-column>
+          <el-table-column prop="detail" label="详情" width="220">
+          </el-table-column>
+          <el-table-column label="操作" align="center" width="300" class-name="small-padding fixed-width">
+              <template #default="scope">
+                  <el-tooltip content="购买" placement="top" v-if="scope.row.userId !== 1">
+                  <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['system:user:edit']"></el-button>
+                  </el-tooltip>
+                  <el-tooltip content="不再推荐" placement="top" v-if="scope.row.userId !== 1">
+                  <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['system:user:remove']"></el-button>
+                  </el-tooltip>
+              </template>
+          </el-table-column>
+        </el-table>
+      </el-main>
+    </div> -->
+    <el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的 Dialog</el-button>
+
+    <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
+      <el-table :data="gridData">
+        <el-table-column property="date" label="日期" width="150"></el-table-column>
+        <el-table-column property="name" label="姓名" width="200"></el-table-column>
+        <el-table-column property="address" label="地址"></el-table-column>
+      </el-table>
+    </el-dialog>
+  </body>
+</template>
+
+
+<script>
+import * as echarts from 'echarts/core';
+export default {
+    data() {
+      return {
+        gridData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        dialogTableVisible: false,
+        dialogFormVisible: false,
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '120px'
+      };
+    }
+  };
+// export default {
+//   data() {
+//     return {
+//       zFieldName: "elevation", // 设置默认的属性字段名称
+//     }
+//   },
+//   beforeMount() {
+
+//   },
+//   created() {
+    
+//     this.load()
+
+//   },
+//   mounted() {
+//   },
+//   methods: {
+
+//     load(){
+//       const dataInit=[
+//           { 
+//             itemName:'五月普通广告一天',
+//             cost:5,
+//             numberleft:10,
+//             detail:10
+//             },
+//           { 
+//             itemName:'五月头条广告一天',
+//             cost:20,
+//             numberleft:10,
+//             detail:10
+//             },
+//           { 
+//             itemName:'任务置顶卡一天',
+//             cost:6,
+//             numberleft:10,
+//             detail:10
+//             },
+//           { 
+//             itemName:'任务置顶卡七天',
+//             cost:40,
+//             numberleft:10,
+//             detail:10
+//             }
+//             ]
+//         this.tableData=dataInit
+//     }
+//   }
+// }
+</script>
+
+<style>
+html,body,#viewDiv{
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/sharingIncentive/index.vue
@@ -0,0 +1,97 @@
+<script>
+import Step3 from './components/currentsharing.vue'
+import Step1 from './components/dataanalysis.vue'
+import Step4 from './components/incentiveusage.vue'
+import Step2 from './components/mysharing.vue'
+import {computed, onMounted, ref} from "vue";
+import * as echarts from 'echarts/core';
+import {reactive} from 'vue'
+import axios from 'axios';
+export default {
+    components:{Step1,Step2,Step3,Step4},
+}
+</script>
+
+<template>
+    <div>
+        <div class="topicText">
+            <h2>产业链数据共享激励</h2>
+        </div>
+        <el-tabs style="margin-left:20px" @tab-click="handleClick">
+            <el-tab-pane label="共享态势">
+                <el-card class="content-card">
+                    <Step1></Step1>
+                </el-card>
+            </el-tab-pane>
+            <el-tab-pane label="我的共享">
+                <el-card class="content-card">
+                    <Step2></Step2>
+                </el-card>
+            </el-tab-pane>            
+            <el-tab-pane label="当前共享">
+                <el-card class="content-card">
+                    <Step3></Step3>
+                </el-card>
+            </el-tab-pane>            
+            <el-tab-pane label="激励使用">
+                <el-card class="content-card">
+                    <Step4></Step4>
+                </el-card>
+            </el-tab-pane>
+        </el-tabs>
+
+    </div>
+</template>
+
+<style scoped>
+
+
+el-row {
+  margin-bottom: 20px;
+}
+
+el-row:last-child {
+  margin-bottom: 0;
+}
+
+el-col {
+  border-radius: 4px;
+}
+
+grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
+.table {
+  border: #2c3e50;
+  align-content: center;
+}
+
+.topicText {
+  text-align: center;
+  font-family: BlinkMacSystemFont;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+
+.card-header {
+  justify-content: space-between;
+  align-items: center;
+  font-size: 18px;
+  text-align: center;
+}
+
+.box-card {
+  width: 480px;
+}
+
+
+</style>
