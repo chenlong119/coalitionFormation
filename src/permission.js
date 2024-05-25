87af08bf158d1166f4fe59b8f8f34e05f0396863
chenlong119 <128025856+chenlong119@users.noreply.github.com>
2024-05-25 20:38:55 +0800

    Update permission.js

--- src/permission.js
+++ src/permission.js
@@ -55,7 +55,7 @@ router.beforeEach((to, from, next) => {
       // 在免登录白名单，直接进入
       next()
     } else {
-      login('clchy', 'clchy1778').then((res)=>{
+      login('admin', 'admin123').then((res)=>{
         setToken(res.token)
         useUserStore().getInfo().then(() => {
           usePermissionStore().generateRoutes().then(accessRoutes => {
